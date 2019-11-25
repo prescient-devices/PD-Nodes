a833aa94d9cb305bce8641700f2eca46b406b4cc
Ashish Yadav <ashish.yadav@prescientdevices.com>
2019-11-25 12:39:48 -0800

    updated revpidio.py out node

--- revpi-dio/revpidio.py
+++ revpi-dio/revpidio.py
@@ -10,17 +10,12 @@ if len(sys.argv) > 2:
     if cmd=="in":
         rpi_in = revpimodio2.RevPiModIO(autorefresh=True,direct_output=True)
         rpi_in.handlesignalend()
-        #rpi.mainloop()
         rpi_in.io[pin_input].reg_event(eventfunction)
-        #print(rpi.io[pin_input].value)
         rpi_in.mainloop()
     if cmd=="out":
         output_value = sys.argv[3]
-        # rpi_out = revpimodio2.RevPiModIO(autorefresh=True,direct_output=True)
-        # rpi_out.handlesignalend()
         rpi_out = revpimodio2.RevPiModIO(autorefresh=True,direct_output=True)
-        rpi_out.io.O_2.value=int(output_value)
-        # rpi_out.mainloop()
+        rpi_out.io[pin_input].value=int(output_value)
         print(int(output_value))
     else:
         print("cmd not found-something is missing!")
