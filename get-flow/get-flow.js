0174c17cfcf9e67c6e0d2da7aa68a64e843e5cb4
Ashish Yadav <ashish.yadav@prescientdevices.com>
2020-04-09 17:11:43 -0700

    New node to get flow file from device (only works on root directory with filename - flows_edge.json) - Pending make it universal

---
+++ get-flow/get-flow.js
@@ -0,0 +1,19 @@
+module.exports = function (RED) {
+    function GetFlow(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+            var currentPath = process.cwd();
+            var fs = require('fs');
+            fs.readFile(process.cwd() + "/.node-red/flows_edge.json", function (err, data) {
+                if (err)
+                    node.send(err)
+                else {
+                    msg.payload = data.toString();
+                    node.send(msg);
+                }
+            });
+        });
+    }
+    RED.nodes.registerType("get-flow", GetFlow);
+}
